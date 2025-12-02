d26a2e7f9827304e28121feddc96b82040d6183d
Pedro Alejandro Ortiz Gilli <pedroortizgilli295@gmail.com>
2025-12-01 22:42:29 -0300

    POST para registrar ventas

--- index.js
+++ index.js
@@ -161,6 +161,48 @@ app.post("/logout", (req, res) => {
     });
 });
 
+//Endpoint para registrar ventas
+app.post("/api/sales", async (req, res) => {
+    try{
+        const { nombreUsuario, precioTotal, fechaEmision, productos } = req.body;
+
+        if(!nombreUsuario || !precioTotal || !fechaEmision || !Array.isArray(productos) || productos.length === 0){
+            return res.status(400).json({
+                message: "Datos inválidos: faltan campos o formato incorrecto"
+            });
+        }
+
+        const sql = `
+            INSERT INTO tickets (nombreUsuario, precioTotal, fechaEmision)
+            VALUES (?, ?, ?)    
+        `;
+
+        const [resultTicket] = await connection.query(sql, [nombreUsuario, precioTotal, fechaEmision]);
+
+        const ticketId = resultTicket.insertId;
+
+        const sqlProductoTickets = `
+            INSERT INTO productos_tickets (idProducto, idTicket) VALUES (?, ?)
+        `;
+
+        for (const idProducto of productos) {
+            await connection.query(sqlProductoTickets, [idProducto, ticketId]);
+        }
+
+        res.status(201).json({
+            message: "Venta registrada con éxito",
+            ticketId: ticketId
+        })
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error interno del servidor",
+            error: error.message
+        });
+    }
+});
+
 
 /*==================
     Iniciar Servidor
