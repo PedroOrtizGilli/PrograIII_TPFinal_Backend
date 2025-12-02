7ab9fa5cfd70e4c1bf2a3c897929d204aee6052a
Pedro Alejandro Ortiz Gilli <pedroortizgilli295@gmail.com>
2025-12-02 11:11:39 -0300

    checkUser

--- src/api/models/user.models.js
+++ src/api/models/user.models.js
@@ -6,6 +6,19 @@ const insertUser = (correo, contrasenia) => {
     return connection.query(sql, [correo, contrasenia]);
 }
 
+const checkUser = async (correo) => {
+    const sqlCheck = "SELECT * FROM usuarios WHERE correo = ?";
+    const [existingUsers] = await connection.query(sqlCheck, [correo]);
+
+    // Si la consulta devuelve algún resultado, es que ya existe
+    if (existingUsers.length > 0) {
+        return res.status(409).json({ 
+            message: "El correo ya está registrado"
+        });
+    }
+}
+
 export default {
-    insertUser
+    insertUser,
+    checkUser
 }
